d8033a4ec54d9e5bae4914c1118ace3eed4d2638
Owen Chase <ochase10@gmail.com>
2025-02-05 15:57:05 -0600

    Add file for showing how checking input could work

---
+++ Week_03/factorial_checkinput.py
@@ -0,0 +1,20 @@
+#get input from the user
+good = False
+while not good:
+	n = input("Please enter a number: ")
+	try:
+		n = int(n)
+	except:
+		continue	
+	good = True
+
+#initialize a running product
+prod = 1
+
+#loop over range(n) gives 0,1,2,...,n-1
+for x in range(n):
+	#x+1 because we want to multiply 1,2,3,...,n
+	prod *= x+1 
+
+#print the result in a good looking way
+print(f"{n}! = {prod}")
